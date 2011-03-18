d62e75cc1eccced629f8a9675a96cbb10bfd9168
Matt Fichman <mfichman@stanford.edu>
2011-03-18 12:42:06 -0700

    Update

---
+++ include/SFR/NullFunctor.hpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * Simple, Fast Renderer (SFR)                                               *
+ * CS249b                                                                    *
+ * Matt Fichman                                                              *
+ * February, 2011                                                            *
+ *****************************************************************************/
+#pragma once
+
+#include "SFR/Common.hpp"
+#include "SFR/Node.hpp"
+
+namespace SFR {
+
+/* Empty functor implementation for performance testing */
+class NullFunctor : public Node::Functor {
+public:
+    void operator()(World* world);
+    void operator()(Camera* camera);
+    void operator()(Transform* transform);
+    void operator()(PointLight* light);
+    void operator()(Model* object);
+    void operator()(HemiLight* light);
+    void operator()(SpotLight* light);
+};
+
+
+}
---
+++ src/NullFunctor.cpp
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * Simple, Fast Renderer (SFR)                                               *
+ * CS249b                                                                    *
+ * Matt Fichman                                                              *
+ * February, 2011                                                            *
+ *****************************************************************************/
+
+#include "SFR/Common.hpp"
+#include "SFR/NullFunctor.hpp"
+#include "SFR/World.hpp"
+
+using namespace SFR;
+
+
+void NullFunctor::operator()(World* world) {
+    world->root()->operator()(this);
+}
+
+void NullFunctor::operator()(Camera* camera) {
+}
+
+void NullFunctor::operator()(Transform* transform) {
+    for (Iterator<Node> node = transform->children(); node; node++) {
+        node(this);
+    }
+}
+
+void NullFunctor::operator()(PointLight* light) {
+}
+
+void NullFunctor::operator()(Model* object) {
+}
+
+void NullFunctor::operator()(HemiLight* light) {
+}
+
+void NullFunctor::operator()(SpotLight* light) {
+}
