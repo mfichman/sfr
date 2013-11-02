b9c74335d4eec15f9025a95ddb12868230d24734
Matt Fichman <matt.fichman@gmail.com>
2013-11-02 12:57:50 -0400

    Added sfr.hpp

---
+++ include/sfr/sfr.hpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2013 Matt Fichman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, APEXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "AssetTable.hpp"
+#include "AttributeBuffer.hpp"
+#include "Camera.hpp"
+#include "Color.hpp"
+#include "Common.hpp"
+#include "DeferredRenderTarget.hpp"
+#include "DeferredRenderer.hpp"
+#include "DepthRenderTarget.hpp"
+#include "Effect.hpp"
+#include "EffectLoader.hpp"
+#include "FlatRenderer.hpp"
+#include "Frustum.hpp"
+#include "HemiLight.hpp"
+#include "IndexBuffer.hpp"
+#include "Instance.hpp"
+#include "Interface.hpp"
+#include "Iterator.hpp"
+#include "LightRenderer.hpp"
+#include "Material.hpp"
+#include "MaterialRenderer.hpp"
+#include "Matrix.hpp"
+#include "Mesh.hpp"
+#include "MeshVertex.hpp"
+#include "Model.hpp"
+#include "Node.hpp"
+#include "NullFunctor.hpp"
+#include "PointLight.hpp"
+#include "Ptr.hpp"
+#include "Quaternion.hpp"
+#include "Shader.hpp"
+#include "ShadowRenderer.hpp"
+#include "SpotLight.hpp"
+#include "TexCoord.hpp"
+#include "Texture.hpp"
+#include "TextureLoader.hpp"
+#include "TextureRenderer.hpp"
+#include "Transform.hpp"
+#include "TransformUpdater.hpp"
+#include "TransparencyRenderer.hpp"
+#include "Vector.hpp"
+#include "WavefrontLoader.hpp"
+#include "World.hpp"
