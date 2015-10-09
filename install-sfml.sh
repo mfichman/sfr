29d31861a9ee3900d9c0edd19d74021fa57041b7
Matt Fichman <matt.fichman@gmail.com>
2015-10-09 12:56:20 -0400

    Enable sudo

--- install-sfml.sh
+++ install-sfml.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget http://www.sfml-dev.org/files/SFML-2.2-sources.zip
 unzip SFML-2.2-sources.zip
-cd SFML-2.2 && cmake -G "Unix Makefiles" && make && make install
+cd SFML-2.2 && cmake -G "Unix Makefiles" && make && sudo make install
