f9fc80a58622cc55b5c8a212288e0816fc17cfe6
Matt Fichman <matt.fichman@gmail.com>
2015-10-09 12:43:20 -0400

    Install SFML from source

--- install-sfml.sh
+++ install-sfml.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-wget http://www.sfml-dev.org/files/SFML-2.3-linux-gcc-64-bit.tar.gz
-tar -xzvf SFML-2.3-linux-gcc-64-bit.tar.gz
-cp SFML-2.3/lib/* /usr/lib/x86_64-linux-gnu/
-cp -r SFML-2.3/include/SFML /usr/include/
+wget http://www.sfml-dev.org/files/SFML-2.3-sources.zip
+unzip SFML-2.3-sources.zip
+cd SFML-2.3 && cmake -G "Unix Makefiles" && make && make install
